379cf24a08c065ac8109f7fdc1290c7e995187cb
ch <chaueur@student.42.fr>
2017-11-07 12:48:07 +0100

    Optimizes sample count calculation

--- src/environment.c
+++ src/environment.c
@@ -6,7 +6,7 @@
 /*   By: chaueur <chaueur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/26 16:12:06 by chaueur           #+#    #+#             */
-/*   Updated: 2017/11/07 12:02:01 by chaueur          ###   ########.fr       */
+/*   Updated: 2017/11/07 12:45:46 by chaueur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void				init_environment(t_env **e)
 	*e = malloc(sizeof(t_env));
 	(*e)->win.width = 0;
 	(*e)->win.height = 0;
-	(*e)->samp_rate = 1;
+	(*e)->samp_rate = 16;
 	(*e)->cam = NULL;
 	(*e)->lights = NULL;
 	(*e)->geos = NULL;

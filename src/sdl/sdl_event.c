a309a894a365716d70def18eec372e01d27bb13e
ch <chaueur@student.42.fr>
2017-11-30 21:54:08 +0100

    Fixes segfault on save

--- src/sdl/sdl_event.c
+++ src/sdl/sdl_event.c
@@ -6,7 +6,7 @@
 /*   By: shirese <shirese@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/16 18:41:29 by chaueur           #+#    #+#             */
-/*   Updated: 2017/11/30 19:01:57 by shirese          ###   ########.fr       */
+/*   Updated: 2017/11/30 21:53:38 by shirese          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,47 @@
 #include "rt.h"
 #include "rt_multithread.h"
 
+static void			sdl_save_img_norme(unsigned char *px, SDL_Surface *info_surface)
+{
+	SDL_Surface		*save_surface;
+
+	save_surface = SDL_CreateRGBSurfaceFrom(px, info_surface->w, \
+		info_surface->h, info_surface->format->BitsPerPixel, \
+		info_surface->w * info_surface->format->BytesPerPixel, \
+		info_surface->format->Rmask, info_surface->format->Gmask, \
+		info_surface->format->Bmask, info_surface->format->Amask);
+	if (save_surface == NULL)
+	{
+		ft_printf("SDL_CreateRGBSurfaceFrom failed: %s\n", SDL_GetError());
+		free(px);
+		return ;
+	}
+	SDL_SaveBMP(save_surface, "saves/save.bmp");
+	free(px);
+	SDL_FreeSurface(save_surface);
+}
+
 static void			sdl_save_img(t_env *e)
 {
 	SDL_Surface		*info_surface;
-	SDL_Surface		*save_surface;
 	int				w;
 	int				h;
+	unsigned char	*pixels;
 
 	if (!(info_surface = SDL_GetWindowSurface(e->win.handle)))
 	{
 		ft_printf("SDL_GetWindowSurface failed: %s\n", SDL_GetError());
 		return ;
 	}
-	unsigned char * pixels[info_surface->w * info_surface->h * info_surface->format->BytesPerPixel];
-	if (SDL_RenderReadPixels(e->win.rend, &info_surface->clip_rect, info_surface->format->format, pixels, info_surface->w * info_surface->format->BytesPerPixel) != 0) {
-		ft_printf("fe,wlfew");
-		return ;
-	}
-	save_surface = SDL_CreateRGBSurfaceFrom(pixels, info_surface->w, info_surface->h, info_surface->format->BitsPerPixel, info_surface->w * info_surface->format->BytesPerPixel, info_surface->format->Rmask, info_surface->format->Gmask, info_surface->format->Bmask, info_surface->format->Amask);
-	if (save_surface == NULL)
+	pixels = malloc(sizeof(unsigned char) * info_surface->w * info_surface->h \
+		* info_surface->format->BytesPerPixel);
+	if (SDL_RenderReadPixels(e->win.rend, &info_surface->clip_rect, info_surface->format->format, pixels, info_surface->w * info_surface->format->BytesPerPixel) != 0)
 	{
-		ft_printf("SDL_CreateRGBSurfaceFrom failed: %s\n", SDL_GetError());
+		ft_printf("SDL_RenderReadPixels failed: %s\n", SDL_GetError());
+		free(pixels);
 		return ;
 	}
-	SDL_SaveBMP(save_surface, "save.bmp");
-	SDL_FreeSurface(save_surface);
+	sdl_save_img_norme(pixels, info_surface);
 }
 
 static int			check_trans_event(SDL_Keycode k, t_env *e)

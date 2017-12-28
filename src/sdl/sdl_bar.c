b5f7bf916c67f3bbcc32921a7a1c9ada14bd07e3
ch <chaueur@student.42.fr>
2017-12-28 17:36:46 +0100

    Fixes reflection for mirrors and loading bar performance

--- src/sdl/sdl_bar.c
+++ src/sdl/sdl_bar.c
@@ -6,22 +6,34 @@
 /*   By: chaueur <chaueur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/27 12:21:10 by chaueur           #+#    #+#             */
-/*   Updated: 2017/12/28 14:48:21 by chaueur          ###   ########.fr       */
+/*   Updated: 2017/12/28 17:36:03 by chaueur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 #include "sdl_func.h"
 
+static void			fill_loading_bar(float ld, SDL_Rect r, t_env *e)
+{
+	int				width;
+
+	width = (e->win.w * ld) / 10;
+	r.w = width;
+	SDL_SetRenderDrawColor(e->win.rend, 0, 0, 0, 255 );
+	SDL_RenderClear(e->win.rend);
+	SDL_SetRenderDrawColor(e->win.rend, ld * 255, 0, 1 - ld * 255, 255);
+	SDL_RenderFillRect(e->win.rend, &r);
+	SDL_RenderPresent(e->win.rend);
+}
+
 void				render_loading_bar(int tile, int tiles_num, t_env *e)
 {
-	static int		loading_done;
+	static int		ld_done;
 	static float	max_count;
 	static SDL_Rect	r;
-	int				width;
-	float			progress;
+	float			ld;
 
-	if (loading_done)
+	if (ld_done)
 		return ;
 	if (!max_count)
 	{
@@ -30,15 +42,9 @@ void				render_loading_bar(int tile, int tiles_num, t_env *e)
 		r.y = e->win.h / 2 - 10;
 		r.h = 20;
 	}
-	progress = tile * max_count;
-	width = (e->win.w * progress) / 10;
-	r.w = width;
-	SDL_SetRenderDrawColor(e->win.rend, 0, 0, 0, 255 );
-	SDL_RenderClear(e->win.rend);
-	SDL_SetRenderDrawColor(e->win.rend, progress * 255, 0, \
-		1 - progress * 255, 255);
-	SDL_RenderFillRect(e->win.rend, &r);
-	SDL_RenderPresent(e->win.rend);
-	if (progress >= 0.990000)
-		loading_done = 1;
+	ld = tile * max_count;
+	if ((int)(ld * 100) % 15 == 0)
+		fill_loading_bar(ld, r, e);
+	if (ld >= 0.990000)
+		ld_done = 1;
 }
